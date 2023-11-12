903beefd8fe8d81854fc06f5581026a0adda01f0
synnph24852 <synnph24852@fpt.edu.vn>
2023-11-13 00:53:49 +0700

    push sale

--- src/layout/Client/ClientLayout.tsx
+++ src/layout/Client/ClientLayout.tsx
@@ -1,16 +1,23 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import Header from "./Header";
+import Footer from "./Footer";
 
-type Props = {};
+const ClientLayout = () => {
+    return (
+        <>
+            <div className="h-8 flex">
+                <span className="m-auto text-[rgb(68,68,68)] text-sm">Hotline: 0903.150.443 | Free Ship cho đơn hàng trên 1tr đồng</span>
+            </div>
+            <Header />
 
-const ClientLayout = (props: Props) => {
-  return (
-    <div>
-      Header
-      <Outlet />
-      Footer
-    </div>
-  );
+            <main>
+                <Outlet />
+            </main>
+
+            <Footer />
+        </>
+    );
 };
 
 export default ClientLayout;
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,79 +1,40 @@
 import { createBrowserRouter } from "react-router-dom";
-// import { MyComponent } from "./components";
 import { AdminLayout, ClientLayout } from "./layout";
-import Product from "./pages/admin/Product/Product"
+import SaleAdminPage from "./pages/Admin/SaleAdminPage";
+import HomePage from "./pages/Main/HomePage";
+import ProductDetailPage from "./pages/Main/ProductDetailPage";
 
-import AddProduct from "./pages/admin/Product/AddProduct"
-import Size from "./pages/admin/size/size"
-import AddSize from "./pages/admin/size/AddSize"
-import UpdateProduct from "./pages/admin/Product/UpdateProduct";
-import RecycleBin from "./pages/admin/Product/recycle-bin";
-import UpdateSize from "./pages/admin/size/UpdateSize";
-import ImageProduct from "./pages/admin/imageProduct/imageProduct"
-import AddImage from "./pages/admin/imageProduct/AddImage";
 export const routers = createBrowserRouter([
-  {
-    path: "/",
-    element: <ClientLayout />,
-    children: [
-      {
-        // index: true,
-        // element: <MyComponent />,
-      },
-    ],
-  },
-  {
-    path: "/admin",
-    element: <AdminLayout />,
-    children: [
-      {
-        path: "product",
-        element: <Product products={[]}/>,
-      },
-      {
-        path: "product/add",
-        element: <AddProduct/>,
-  
-      },
-      {
-        path: "product/update/:id",
-        element: <UpdateProduct/>,
-      },
-      {
-        path: "product/recycle",
-        element: <RecycleBin/>,
-      },
-      {
-        path: "size",
-        element: <Size/>,
-      },
-      {
-        path: "size/add",
-        element: <AddSize/>,
-      },
-      {
-        path: "size/update/:id",
-        element: <UpdateSize/>,
-      },
-      {
-        path: "imageProduct",
-        element: <ImageProduct/>,
-      },
-      {
-        path: "imageProduct/add",
-        element: <AddImage/>,
-      },
-      // {
-      //   path: "size/update/:id",
-      //   element: <UpdateSize/>,
-      // },
-
-     
-      
-    ],
-  },
-  {
-    path: "*",
-    element: "NotFound Page nhé",
-  },
+    {
+        path: "/",
+        element: <ClientLayout />,
+        children: [
+            {
+                index: true,
+                element: <HomePage />,
+            },
+            {
+                path: "/products/:id",
+                element: <ProductDetailPage />,
+            },
+        ],
+    },
+    {
+        path: "/admin",
+        element: <AdminLayout />,
+        children: [
+            {
+                index: true,
+                element: "Page admin here",
+            },
+            {
+                path: "sale",
+                element: <SaleAdminPage />,
+            },
+        ],
+    },
+    {
+        path: "*",
+        element: "NotFound Page nhé",
+    },
 ]);
